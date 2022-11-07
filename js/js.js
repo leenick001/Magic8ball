49f72a3d696f0bf72c0057765873ea506d2fc490
Vlatko Stojkoski <stojvlatko@gmail.com>
2022-11-07 08:12:53 +0000

    Pending changes exported from your codespace

--- js/js.js
+++ js/js.js
@@ -1,28 +1,8 @@
-// JavaScript Document
-function generate_random(max_number) {
-	return Math.round(Math.random()*max_number);
-}
+const button = document.querySelector("button");
+const answer = document.querySelector("#answer");
 
-var button = document.querySelector("button");
-var answer = document.querySelector("#answer");
+button.addEventListener("click", function () {
+	var answerText = Math.random() < 0.5 ? "Сара вели ДА" : "Сара вели не";
 
-button.addEventListener("click", function() {
-	
-	
-	var randomNumber = generate_random(3);
-	
-	var answerText = "";
-	
-	  if (randomNumber == 0) {
-		  answerText = "Yes definitely";
-	  }  else if (randomNumber == 1) {
-	     answerText = "No certainly not";
-		 
-	   } else if (randomNumber == 2) {
-		   answerText = "Ask again later";
-		   
-	   } else {
-		   answerText = "I really don't care";
-	   }
-	   answer.innerHTML = answerText;
+	answer.innerHTML = answerText;
 });
